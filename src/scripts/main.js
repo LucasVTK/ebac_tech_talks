2ff6bcd249a2f7106aa2c96f9ae388e5109e845a
LucasVTK <lucazim.vtk@gmail.com>
2025-02-15 21:27:48 -0300

    criando a contagem regressiva

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+const dataDoEvento = new Date("dec 2, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento/ diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras)
+        document.getElementById('contador').innerHTML = `<br> O evento ja encerrou`
+    }
+
+
+}, 1000);
